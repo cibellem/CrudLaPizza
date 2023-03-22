6b28a1417ccdd8445ef5c767dc467e69276ef81b
Cibelle  Montor <montorcibelle@gmail.com>
2023-03-22 00:27:22 -0700

    UI combined

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,13 @@
 import Toppings from "./Toppings";
 import Pizzas from "./Pizzas";
-import { Tab } from "semantic-ui-react";
 
 function App() {
-  const panes = [
-    {
-      menuItem: "Toppings",
-      render: () => (
-        <Tab.Pane>
-          <Toppings />
-        </Tab.Pane>
-      ),
-    },
-    {
-      menuItem: "Pizzas",
-      render: () => (
-        <Tab.Pane>
-          <Pizzas />
-        </Tab.Pane>
-      ),
-    },
-  ];
-
-  return <Tab panes={panes} />;
+  return (
+    <>
+      <Toppings />
+      <Pizzas />
+    </>
+  );
 }
 
 export default App;
